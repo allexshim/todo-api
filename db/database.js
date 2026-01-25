2ca0968e13029074b8cf93d177019fbe912709bf
Shim <yeeun0012@gmail.com>
2026-01-25 18:32:01 +0900

    fix: add DB env

--- db/database.js
+++ db/database.js
@@ -1,13 +1,17 @@
 import { Pool } from "pg";
 import { config } from "../config.js";
 
-export const pool = new Pool({
-  host: config.db.host,
-  user: config.db.user,
-  database: config.db.name,
-  password: config.db.password,
-  port: config.db.port,
-});
+const connectionString = process.env.DATABASE_URL;
+
+export const pool = new Pool(
+  connectionString ?? {
+    host: config.db.host,
+    user: config.db.user,
+    database: config.db.name,
+    password: config.db.password,
+    port: config.db.port,
+  },
+);
 
 export async function query(text, params = []) {
   const res = await pool.query(text, params);
