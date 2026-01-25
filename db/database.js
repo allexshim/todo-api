4b565a973c210737bdfab841d0660437de71e8b6
Shim <yeeun0012@gmail.com>
2026-01-25 20:00:10 +0900

    fix: bug

--- db/database.js
+++ db/database.js
@@ -3,15 +3,18 @@ import { config } from "../config.js";
 
 const connectionString = process.env.DATABASE_URL;
 
-export const pool = new Pool(
-  connectionString ?? {
-    host: config.db.host,
-    user: config.db.user,
-    database: config.db.name,
-    password: config.db.password,
-    port: config.db.port,
-  },
-);
+export const pool = connectionString
+  ? new Pool({
+      connectionString,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      host: config.db.host,
+      user: config.db.user,
+      database: config.db.name,
+      password: config.db.password,
+      port: config.db.port,
+    });
 
 export async function query(text, params = []) {
   const res = await pool.query(text, params);
